a32557a73b1ae0603a32aa2e8f4f523a4510fab9
Aleksey.Sanikovich <vseokdog@gmail.com>
2016-05-18 22:58:24 +0300

    finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "alsan", password: "alsan", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
